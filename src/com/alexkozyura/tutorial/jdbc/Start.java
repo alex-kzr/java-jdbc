e125f039d3a15acecdcb1550215437b27d2a9653
Alex-SK <aleksey.kozyura@gmail.com>
2020-10-30 15:25:58 +1000

    Add file with DB config and sql requests

--- src/com/alexkozyura/tutorial/jdbc/Start.java
+++ src/com/alexkozyura/tutorial/jdbc/Start.java
@@ -1,7 +1,9 @@
 package com.alexkozyura.tutorial.jdbc;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -18,7 +20,48 @@ public class Start {
             String dbPath = scanner.nextLine();
             System.out.println("DB path: " + dbPath);
 
-            // StringBuilder stringBuilder = new StringBuilder();
+            if(!new File(dbPath).exists()){
+                System.out.println("Файл базы данных не найден!");
+                return;
+            }
+
+            StringBuilder requestsBuilder = new StringBuilder();
+            try {
+                while (scanner.hasNextLine()){
+                    requestsBuilder.append(scanner.nextLine());
+                }
+            } finally {
+                scanner.close();
+            }
+
+            String[] sqlRequests = requestsBuilder.toString().split(";");
+            for (int i = 0; i < sqlRequests.length; i++) {
+                sqlRequests[i] = sqlRequests[i] + ";";
+            }
+
+            StringBuilder resultBuilder = new StringBuilder();
+
+            DBUtils.openConnection(dbPath);
+
+            for (String sqlRequest : sqlRequests){
+                resultBuilder.append("Request : " + sqlRequest + "\n");
+                resultBuilder.append("Response : " + "\n");
+                TableObject table = DBUtils.getResultTable(sqlRequest);
+
+                for(String header : table.getHeaders()){
+                    resultBuilder.append(header + ", ");
+                }
+                resultBuilder.append("\n");
+
+                for(TableRow tableRow: table.getTableRows()){
+                    for(Object cell : tableRow.getTableRow()){
+                        resultBuilder.append(cell + ", ");
+                    }
+                    resultBuilder.append("\n");
+                }
+            }
+
+            System.out.println(resultBuilder);
 
         } catch (Exception e){
 
@@ -34,12 +77,7 @@ public class Start {
 //
 //        try {
 //
-//            // dynamic sqlite driver registration
-//            Driver driver = (Driver) Class.forName("org.sqlite.JDBC").newInstance();
-//            // DriverManager.registerDriver(driver);
 //
-//            // create connection to DB
-//            String url = "jdbc:sqlite:e:\\Docs\\workspace\\Java\\java-jdbc\\car-shop.db";
 //            connection = DriverManager.getConnection(url);
 //
 //            // prepare SQL request
@@ -49,24 +87,7 @@ public class Start {
 //            // execute SQL request
 //            resultSet = statement.executeQuery(sql);
 //
-//            // process result
-//            ResultSetMetaData metaData = resultSet.getMetaData();
-//            int columnCount = metaData.getColumnCount();
 //
-//            String tableHeader = "";
-//            for(int i = 1; i <= columnCount; i++){
-//                tableHeader += metaData.getColumnName(i) + ", ";
-//            }
-//            System.out.println(tableHeader);
-//
-//            while (resultSet.next()){
-//
-//                String tableRow = "";
-//                for (int i = 1; i <= columnCount; i++){
-//                    tableRow += resultSet.getString(i) + ", ";
-//                }
-//                System.out.println(tableRow);
-//            }
 //
 //        } catch (SQLException e){
 //            e.printStackTrace();
