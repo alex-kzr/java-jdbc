905334f0226db5340952a71703cf9d87d2dde41e
Alex-SK <aleksey.kozyura@gmail.com>
2020-10-30 12:20:01 +1000

    Move db path and sql request to file

--- src/com/alexkozyura/tutorial/jdbc/Start.java
+++ src/com/alexkozyura/tutorial/jdbc/Start.java
@@ -1,6 +1,8 @@
 package com.alexkozyura.tutorial.jdbc;
 
+import java.io.FileInputStream;
 import java.sql.*;
+import java.util.Scanner;
 
 /**
  * Created by Alex Kozyura (mail@alexkozyura.com) on 30.10.2020
@@ -9,58 +11,75 @@ import java.sql.*;
 public class Start {
     public static void main(String[] args){
 
-        Connection connection = null; // connection to db
-        Statement statement = null; // store and execute sql requests
-        ResultSet resultSet = null; // get responses result
-
         try {
 
-            // dynamic sqlite driver registration
-            Driver driver = (Driver) Class.forName("org.sqlite.JDBC").newInstance();
-            // DriverManager.registerDriver(driver);
+            Scanner scanner = new Scanner(new FileInputStream("sql.txt"), "UTF-8");
 
-            // create connection to DB
-            String url = "jdbc:sqlite:e:\\Docs\\workspace\\Java\\java-jdbc\\car-shop.db";
-            connection = DriverManager.getConnection(url);
+            String dbPath = scanner.nextLine();
+            System.out.println("DB path: " + dbPath);
 
-            // prepare SQL request
-            String  sql = "SELECT * FROM dict_model";
-            statement = connection.createStatement();
+            // StringBuilder stringBuilder = new StringBuilder();
 
-            // execute SQL request
-            resultSet = statement.executeQuery(sql);
-
-            // process result
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            String tableHeader = "";
-            for(int i = 1; i <= columnCount; i++){
-                tableHeader += metaData.getColumnName(i) + ", ";
-            }
-            System.out.println(tableHeader);
-
-            while (resultSet.next()){
-
-                String tableRow = "";
-                for (int i = 1; i <= columnCount; i++){
-                    tableRow += resultSet.getString(i) + ", ";
-                }
-                System.out.println(tableRow);
-            }
-
-        } catch (SQLException e){
-            e.printStackTrace();
         } catch (Exception e){
+
             e.printStackTrace();
-        } finally {
-            try {
-                if (resultSet!=null) resultSet.close();
-                if (statement!=null) statement.close();
-                if (connection!=null) statement.close();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
         }
+
+
+
+
+//        Connection connection = null; // connection to db
+//        Statement statement = null; // store and execute sql requests
+//        ResultSet resultSet = null; // get responses result
+//
+//        try {
+//
+//            // dynamic sqlite driver registration
+//            Driver driver = (Driver) Class.forName("org.sqlite.JDBC").newInstance();
+//            // DriverManager.registerDriver(driver);
+//
+//            // create connection to DB
+//            String url = "jdbc:sqlite:e:\\Docs\\workspace\\Java\\java-jdbc\\car-shop.db";
+//            connection = DriverManager.getConnection(url);
+//
+//            // prepare SQL request
+//            String  sql = "SELECT * FROM dict_model";
+//            statement = connection.createStatement();
+//
+//            // execute SQL request
+//            resultSet = statement.executeQuery(sql);
+//
+//            // process result
+//            ResultSetMetaData metaData = resultSet.getMetaData();
+//            int columnCount = metaData.getColumnCount();
+//
+//            String tableHeader = "";
+//            for(int i = 1; i <= columnCount; i++){
+//                tableHeader += metaData.getColumnName(i) + ", ";
+//            }
+//            System.out.println(tableHeader);
+//
+//            while (resultSet.next()){
+//
+//                String tableRow = "";
+//                for (int i = 1; i <= columnCount; i++){
+//                    tableRow += resultSet.getString(i) + ", ";
+//                }
+//                System.out.println(tableRow);
+//            }
+//
+//        } catch (SQLException e){
+//            e.printStackTrace();
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (resultSet!=null) resultSet.close();
+//                if (statement!=null) statement.close();
+//                if (connection!=null) statement.close();
+//            } catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
