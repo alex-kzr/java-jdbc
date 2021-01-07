e9ad1591ae38ba50059b41dcb77f545a38b49ecd
Alex-SK <aleksey.kozyura@gmail.com>
2021-01-08 01:59:02 +1000

    Add JFrame

--- src/com/alexkozyura/tutorial/jdbc/form/Main.java
+++ src/com/alexkozyura/tutorial/jdbc/form/Main.java
@@ -1,6 +1,9 @@
 package com.alexkozyura.tutorial.jdbc.form;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.sql.Connection;
@@ -21,6 +24,14 @@ public class Main {
 
             MyTableRenderer cellRenderer = new MyTableRenderer();
             jTable.setDefaultRenderer(Object.class, cellRenderer);
+
+            JScrollPane scroller = new JScrollPane(jTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+            JFrame jFrame = new JFrame("Загрузка данных в JTable");
+            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jFrame.getContentPane().add(scroller);
+            jFrame.pack();
+            jFrame.setVisible(true);
 
         } catch (Exception e) {
             e.printStackTrace();
